e756d39c371e6aa382c045af9696e68080b5f054
SALL Oumar <119567549+oumar146@users.noreply.github.com>
2024-06-03 10:34:52 +0200

    ajout du header t de la banière

---
+++ jo_app/src/components/Banner.jsx
@@ -0,0 +1,16 @@
+import '../styles/banner.css'
+
+
+const Banner = () => {
+
+    return (
+        <div className="bg-banner">
+            <h1>
+                PARIS 2024
+                LA PLATEFORME DE PARTAGE DE DONNÉES
+            </h1>
+        </div>
+    )
+}
+
+export default Banner;
---
+++ jo_app/src/components/Header.jsx
@@ -0,0 +1,22 @@
+import '../styles/header.css'
+import { NavLink } from 'react-router-dom'
+import logo from '../assets/logo.svg'
+
+const Header = () => {
+
+    return (
+        <header className="bg-header">
+            <NavLink to='/home' className='logo'>
+                <img src={logo} alt='logo JO' className='logo' />
+                <p>Paris 2024 data</p>
+            </NavLink>
+            <ul className='nav-links'>
+                <li>FAQ</li>
+                <li>Inscription</li>
+                <li>Connexion</li>
+            </ul>
+        </header>
+    )
+}
+
+export default Header;
